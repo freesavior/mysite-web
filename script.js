9a3a9182ee903c31d6c215975aa06a41af18642a
freesavior <freesavior@users.noreply.github.com>
2024-09-14 02:34:57 +0000

    deploy: 17f50d4990709bcc44963727f0da65e885eafc40

---
+++ script.js
@@ -0,0 +1,46 @@
+// public/script.js
+
+document.getElementById('downloadButton').addEventListener('click', function() {
+    // Rediriger vers la route de téléchargement
+    window.location.href = '/telecharger-le-fichier';
+});
+
+
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const dispoButton = document.getElementById("dispo-button");
+  
+    // Par défaut, le bouton est disponible
+    let isAvailable = true;
+  
+    dispoButton.addEventListener("click", function () {
+      if (isAvailable) {
+        dispoButton.textContent = "Indisponible";
+        dispoButton.classList.remove("btn-disponible");
+        dispoButton.classList.add("btn-indisponible");
+      } else {
+        dispoButton.textContent = "Disponible";
+        dispoButton.classList.remove("btn-indisponible");
+        dispoButton.classList.add("btn-disponible");
+      }
+      isAvailable = !isAvailable; // Inverser l'état
+    });
+  });
+
+
+  let estDisponible = true;
+  
+  dispoButton.addEventListener("click", function () {
+    if (estDisponible) {
+      dispoButton.textContent = "Indisponible";
+      dispoButton.classList.remove("btn-disponible");
+      dispoButton.classList.add("btn-indisponible");
+    } else {
+      dispoButton.textContent = "Disponible";
+      dispoButton.classList.remove("btn-indisponible");
+      dispoButton.classList.add("btn-disponible");
+    }
+    estDisponible = !estDisponible; // Inverser l'état
+  });
+});
