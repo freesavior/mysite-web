590938808620bfa67a86d7199a8204ee53cf5138
freesavior <omarbaraze@MacBook-Pro-de-Omar.local>
2024-09-16 01:38:44 +0200

    Configuration du projet pour déploiement sur Heroku

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
