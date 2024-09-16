dc6bed52b429b18b6335e9f86448a8dc63a8e65b
freesavior <omarbaraze@MacBook-Pro-de-Omar.local>
2024-09-16 02:45:21 +0200

    Configuration du projet pour déploiement sur Heroku

--- public/script.js
+++ public/script.js
@@ -1,28 +1,28 @@
-// Gestion du téléchargement du fichier
-document.getElementById('downloadButton').addEventListener('click', function() {
-  // Rediriger vers la route de téléchargement
-  window.location.href = '/telecharger-le-fichier';
-});
+// // Gestion du téléchargement du fichier
+// document.getElementById('downloadButton').addEventListener('click', function() {
+//   // Rediriger vers la route de téléchargement
+//   window.location.href = '/telecharger-le-fichier';
+// });
 
 
 
-// Gestion de la disponibilité du bouton
-document.addEventListener("DOMContentLoaded", function () {
-  const dispoButton = document.getElementById("dispo-button");
+// // Gestion de la disponibilité du bouton
+// document.addEventListener("DOMContentLoaded", function () {
+//   const dispoButton = document.getElementById("dispo-button");
 
-  // Par défaut, le bouton est disponible
-  let isAvailable = true;
+//   // Par défaut, le bouton est disponible
+//   let isAvailable = true;
 
-  dispoButton.addEventListener("click", function () {
-    if (isAvailable) {
-      dispoButton.textContent = "Indisponible";
-      dispoButton.classList.remove("btn-disponible");
-      dispoButton.classList.add("btn-indisponible");
-    } else {
-      dispoButton.textContent = "Disponible";
-      dispoButton.classList.remove("btn-indisponible");
-      dispoButton.classList.add("btn-disponible");
-    }
-    isAvailable = !isAvailable; // Inverser l'état
-  });
-});
+//   dispoButton.addEventListener("click", function () {
+//     if (isAvailable) {
+//       dispoButton.textContent = "Indisponible";
+//       dispoButton.classList.remove("btn-disponible");
+//       dispoButton.classList.add("btn-indisponible");
+//     } else {
+//       dispoButton.textContent = "Disponible";
+//       dispoButton.classList.remove("btn-indisponible");
+//       dispoButton.classList.add("btn-disponible");
+//     }
+//     isAvailable = !isAvailable; // Inverser l'état
+//   });
+// });
